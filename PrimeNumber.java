eece2ba3e380f65e3790f68808bae91195ad66c4
learningsubbu <68122504+learningsubbu@users.noreply.github.com>
2020-07-17 22:55:23 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int Input = 13;
+		System.out.println("Printing Prime Numbers:");
+		for (int i=1;i<=Input/2;i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
