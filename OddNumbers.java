8e0331a7836e6a52e1f34f90752216b9b6e9e682
learningsubbu <68122504+learningsubbu@users.noreply.github.com>
2020-07-17 22:52:34 +0530

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Printing Only Odd Numbers ");
+		
+		for (int i=0;i<20;i++) {
+			
+			if(i % 2 != 0)
+			{
+	            System.out.print(i+",");
+	            
+			}
+			
+	}
+		
+	}
+}
